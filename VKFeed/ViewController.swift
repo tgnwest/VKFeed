05b21c5043f61661180a113ef9e4bfe0be3a8440
Alex <tgnwest@gmail.com>
2020-06-01 13:48:00 +0300

    Initial Commit

---
+++ VKFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKFeed
+//
+//  Created by Alex Tregubov on 01.06.2020.
+//  Copyright © 2020 Alex Tregubov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
