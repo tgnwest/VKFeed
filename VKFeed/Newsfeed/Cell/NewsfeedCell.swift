ee3fa579bc6574169d5650f58957097aca3068d1
Alex <tgnwest@gmail.com>
2020-06-03 13:54:45 +0300

    added image
    added image to the post icon

--- VKFeed/Newsfeed/Cell/NewsfeedCell.swift
+++ VKFeed/Newsfeed/Cell/NewsfeedCell.swift
@@ -24,7 +24,7 @@ class NewsfeedCell: UITableViewCell {
     
     static let reuseId = "NewsfeedCell"
     
-    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var iconImageView: WebImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
@@ -41,6 +41,7 @@ class NewsfeedCell: UITableViewCell {
     }
     
     func set(viewModel: FeedCellViewModel) {
+        iconImageView.set(imageURL: viewModel.iconUrlString)
         nameLabel.text = viewModel.name
         dateLabel.text = viewModel.date
         postLabel.text = viewModel.text
